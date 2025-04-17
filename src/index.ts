ec0e48d6872d7be13215e4348e64ddddc75e0e93
Jani Kaartinen <jkaart@gmail.com>
2025-04-17 11:31:54 +0300

    exercises 9.10 - 9.13

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+
+import diagnosesRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 const PORT = 3001;
@@ -8,6 +14,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
---
+++ src/services/patientService.ts
@@ -0,0 +1,32 @@
+import { v1 as uuid } from 'uuid';
+import patientsData from '../../data/patients';
+
+import { Patient, NonSensitivePatients, NewPatient } from '../types';
+
+const patients: Patient[] = patientsData;
+
+const getPatients = (): Patient[] => {
+  return patients;
+};
+
+const getNonSensitivePatients = (): NonSensitivePatients[] => {
+  return patients.map(({ id, name, dateOfBirth, gender, occupation }) => ({
+    id, name, dateOfBirth, gender, occupation
+  }));
+};
+
+const addPatient = (patient: NewPatient): Patient => {
+  const id = uuid();
+  const newPatient = {
+    id,
+    ...patient
+  };
+  patients.push(newPatient);
+  return newPatient;
+};
+
+export default {
+  getPatients,
+  getNonSensitivePatients,
+  addPatient
+};
